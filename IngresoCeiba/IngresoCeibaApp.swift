2c940f13b6fbbc9cb831c7bce27ccf6d1d7d8a4a
Luis David Ortiz <luisdavidortizr@gmail.com>
2022-04-18 17:56:10 -0500

    Initial Commit

---
+++ IngresoCeiba/IngresoCeibaApp.swift
@@ -0,0 +1,17 @@
+//
+//  IngresoCeibaApp.swift
+//  IngresoCeiba
+//
+//  Created by Luis David Ortiz on 18/04/22.
+//
+
+import SwiftUI
+
+@main
+struct IngresoCeibaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
